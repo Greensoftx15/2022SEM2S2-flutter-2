ebd2096904db542adab89f0c4ecddabae7fbbe22
Greensoftx15 <rafael.manchego@unillanos.edu.co>
2022-09-10 16:43:35 -0500

    Calculadora IMC.

---
+++ lib/pages/detalle.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class detallePage extends StatelessWidget {
+  const detallePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalle del calculo"),
+      ),
+    );
+  }
+}
